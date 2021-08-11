27bd79b1b2b34cee24707e1eb4d52f31c7169a4e
Devaraj <devaraj.n@welnesys.com>
2021-08-11 14:05:03 +0530

    Project with different Targets.

---
+++ TestProjDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProjDev
+//
+//  Created by Devaraj N on 11/08/21.
+//  Copyright © 2021 Devaraj N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
